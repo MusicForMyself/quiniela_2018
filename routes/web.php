58ffe6ff452d1b5d76f85770968fc5986e12830b
John Falcon <john@pixel-muffin.com>
2018-06-17 21:43:49 -0500

    Fondoamarello

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,22 @@
         return view('welcome');
     });
     
+    Route::get('/thank-you', function () {
+        return view('thanks');
+    });
+    
+    Route::get('/enter', function () {
+        return view('enter');
+    });
+    
+     Route::post('/enter', function () {
+        return;
+    });
+    
+    Route::get('/fill-picks', function () {
+        return view('quiniela');
+    });
+    
+    Route::get('/results', function () {
+        return view('results');
+    });
